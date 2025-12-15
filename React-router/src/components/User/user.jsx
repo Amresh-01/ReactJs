1e31c4acf44a7be558b7dbe2d2f18a84b9e8c134
Amresh-01 <amreshchaurasiya373@gmail.com>
2025-12-15 16:50:42 +0530

    feat: a no. fo followers can be seen on /user/github-username

--- React-router/src/components/User/user.jsx
+++ React-router/src/components/User/user.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const User = () => {
   const { userId } = useParams();
-  return <div>UserId : {userId}</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchgithubFollwers = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.github.com/users/${userId || "Amresh-01"}`
+        );
+        setData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchgithubFollwers();
+  }, [userId]);
+
+  // useEffect(() => async
+  //   const response = await axios.get(`https://api.github.com/users//Amresh-01`);
+  // }, [third]);
+
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white ">
+      Github followers: {data?.followers ?? "Loading..."}
+    </div>
+  );
 };
 
 export default User;
